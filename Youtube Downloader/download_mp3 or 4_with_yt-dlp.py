b24c962593362fbbe2aeac05cb09338c74bc817b
Ciel Zencorg <5054241040@student.its.ac.id>
2024-10-11 18:05:35 +0700

    Add files via upload

---
+++ Youtube Downloader/download_mp3 or 4_with_yt-dlp.py
@@ -0,0 +1,23 @@
+import yt_dlp
+
+url = input('Input URL here: ')
+format = input('format of file (mp3/mp4): ').upper()
+
+if format == 'MP3':
+    # MP3 Download
+    ydlOpts = {
+        'format': 'bestaudio/best',
+        'outtmpl': r'C:\Users\Syauqi Nabil\Music\audio.%(ext)s'
+    ,
+    }
+elif format == 'MP4':
+    # MP4 Download
+    ydlOpts = {
+        'format': 'best',  
+        'outtmpl': r'C:\Users\Syauqi Nabil\video.%(ext)s',  
+    }
+
+with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+    ydl.download([url])
+
+print("Download selesai!")
