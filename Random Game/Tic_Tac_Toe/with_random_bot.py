e08c626dde4401c1ccc43aaa722ddfe75e11f613
Luminol <5054241040@student.its.ac.id>
2024-10-26 01:06:22 +0700

    Add files via upload

---
+++ Random Game/Tic_Tac_Toe/with_random_bot.py
@@ -0,0 +1,75 @@
+import random
+
+signDictionary = []
+index_list = []
+
+for i in range(9):
+    signDictionary.append(' ')
+
+def print_board():
+    board = f"""
+     {signDictionary[0]} | {signDictionary[1]} | {signDictionary[2]}
+    ---|---|---
+     {signDictionary[3]} | {signDictionary[4]} | {signDictionary[5]} 
+    ---|---|---
+     {signDictionary[6]} | {signDictionary[7]} | {signDictionary[8]} 
+    """
+    print(board)
+
+def take_input(playerName):
+    while True:
+        x = int(input(f'{playerName}: '))
+        x -= 1
+        if 0 <= x < 10:
+            if x in index_list:
+                print('This spot is blocked')
+                continue
+            index_list.append(x)
+            return x
+        print('Please enter a valid number between 1 and 9')
+
+def bot_move():
+    availableMoves = [i for i in range(9) if i not in index_list]
+    move = random.choice(availableMoves)
+    index_list.append(move)
+    return move
+
+def calculate_result(player_1, player_2):
+    if signDictionary[0] == signDictionary[1] == signDictionary[2] == 'X' or signDictionary[3] == signDictionary[4] == signDictionary[5] == 'X' or signDictionary[6] == signDictionary[7] == signDictionary[8] == 'X' or signDictionary[0] == signDictionary[3] == signDictionary[6] == 'X' or signDictionary[1] == signDictionary[4] == signDictionary[7] == 'X' or signDictionary[2] == signDictionary[5] == signDictionary[8] == 'X' or signDictionary[0] == signDictionary[4] == signDictionary[8] == 'X' or signDictionary[2] == signDictionary[4] == signDictionary[6] == 'X':
+        print(f'{player_1} wins!')
+        quit('Thank you for playing the game!')
+    elif signDictionary[0] == signDictionary[1] == signDictionary[2] == 'O' or signDictionary[3] == signDictionary[4] == signDictionary[5] == 'O' or signDictionary[6] == signDictionary[7] == signDictionary[8] == 'O' or signDictionary[0] == signDictionary[3] == signDictionary[6] == 'O' or signDictionary[1] == signDictionary[4] == signDictionary[7] == 'O' or signDictionary[2] == signDictionary[5] == signDictionary[8] == 'O' or signDictionary[0] == signDictionary[4] == signDictionary[8] == 'O' or signDictionary[2] == signDictionary[4] == signDictionary[6] == 'O':
+        print(f'{player_2} wins!')
+        quit('Thank you for playing the game!')
+
+def main():
+    print('Welcome to the tic tac toe game!')
+    player_1 = input('Input your name for Player 1: ')
+    player_2 = input('Input your name for Player 2 (If you want to play against the computer, type "bot"): ')
+    print(f'Thank you, {player_1} and {player_2}. Now, let\'s start playing!')
+    
+    print(f"{player_1}'sign will be - X")
+    print(f"{player_2}'sign will be - O")
+    print('Enter any key to start the game : ')
+
+    print_board( )
+
+    for i in range(9):
+        if i % 2 == 0:
+            index = take_input(player_1)
+            signDictionary[index] = 'X'
+        else:
+            if player_2.lower() == "bot":
+                index = bot_move()
+                signDictionary[index] = 'O'
+                print(f'Bot chose position {index + 1}')
+            else:
+                index = take_input(player_2)
+                signDictionary[index] = 'O'
+
+        print_board()
+        calculate_result(player_1, player_2)
+
+    print('This is a tie, nobody won!')
+main()
+
